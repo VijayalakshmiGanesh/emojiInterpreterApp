a3e5ed36fd5ce7e2bbb31636a52f9d1aaa1eed65
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-06-19 18:09:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+const emojiDict = {
+  "😀": "Grinning Face",
+  "🤗": "Hugging Face",
+  "😖": "Tired Face",
+  "😕": "Confused Face",
+  "😪": "Sleepy Face",
+  "🤣": "Rolling on the floor Laughing",
+  "🥺": "Pleading Face",
+  "😲": "Astonised Face",
+  "🤒": "Sick",
+  "😱": "Screaming in Fear"
+};
+
+var emojiArray = Object.keys(emojiDict);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("Translation will appear here..");
+  const [emoji, setEmoji] = useState("");
+
+  function findMeaning(event) {
+    var input = event.target.value;
+    setEmoji(input);
+
+    if (input in emojiDict) var meaning = emojiDict[input];
+    else if (meaning === undefined) {
+      meaning = "Sorry we do not have this emoji in our Database";
+    } else {
+      setMeaning(meaning);
+      console.log(meaning);
+    }
+  }
+
+  function emojiSelector(item) {
+    var meaning = emojiDict[item];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Meaning Finder</h1>
+      <div class="input-box" style={{ minWidth: "80%" }}>
+        <input onChange={findMeaning} value={emoji}></input>
+      </div>
+      <h2>{emoji} </h2>
+      <h3>{meaning}</h3>
+
+      <div id="emojiArray">
+        Emojis in our DB:
+        {emojiArray.map((item) => {
+          return <span onClick={() => emojiSelector(item)}>{item}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
